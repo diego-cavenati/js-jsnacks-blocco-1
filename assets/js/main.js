b54ae9018b79892de4341c0d5f378d03796cf560
diego-cavenati <diegocavenati@gmail.com>
2022-10-12 11:37:15 +0200

    Snack 1

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Snack 1
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore. 
+
+// numero 1
+const numero1 = prompt("inserisci il primo numero");
+
+// numero 2
+const numero2 = prompt("Inserisci il secondo numero");
+
+if (numero1 > numero2) {
+    console.log(numero1);
+} else if (numero1 < numero2) {
+    console.log(numero2);
+} else {
+    console.log(pari);
+}
