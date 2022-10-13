99b15db6f6771c08a46d2439ebe5d64f99dd3fce
diego-cavenati <diegocavenati@gmail.com>
2022-10-13 12:01:08 +0200

    while

--- jsnack-3/assets/js/main.js
+++ jsnack-3/assets/js/main.js
@@ -1,17 +1,33 @@
 //snack 3
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
  
+// let userNumber = 0;
+
+// const h1Element = document.querySelector("h1");
+
+// for (let i = 0; i < 10; i++) {
+//     //const element = array[i];
+//     userNumber += Number(prompt("inserisci un numero"));
+//     console.log(userNumber);
+// }
+
+// // Il programma stampa la somma di tutti i numeri inseriti.
+// h1Element.innerHTML = `${userNumber}`;
+
+
+// While
 let userNumber = 0;
 
 const h1Element = document.querySelector("h1");
 
-for (let i = 0; i < 10; i++) {
+let i = 0;
+while (i < 10) {
     //const element = array[i];
     userNumber += Number(prompt("inserisci un numero"));
     console.log(userNumber);
+
+    i++
 }
 
 // Il programma stampa la somma di tutti i numeri inseriti.
 h1Element.innerHTML = `${userNumber}`;
-
-
